ebdacfa60083770bbaedff806057e33f8b1f8fa7
lucianolopez85 <lopezz.lucio85@gmail.com>
2022-08-27 21:26:33 -0300

    first screen

---
+++ app/src/main/java/com/example/cadastro_de_usuario/CadastroDeUsuariosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cadastro_de_usuario
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroDeUsuariosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_de_usuarios)
+    }
+}
