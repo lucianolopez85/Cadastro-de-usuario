5ab435bcb788da4621d10eeb09dd75a5ac2ff0c0
lucianolopez85 <lopezz.lucio85@gmail.com>
2022-11-10 22:53:17 -0300

    finalizado endPoint, parametro mockado

--- app/src/main/java/com/example/cadastro_de_usuario/data/dto/PullsListResponseDTO.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cadastro_de_usuario.data.dto
-
-import com.google.gson.annotations.SerializedName
-
-data class PullsListResponseDTO (
-    @SerializedName("pulls") val pulls: List<PullsListDTO>
-)
---
+++ app/src/main/java/com/example/cadastro_de_usuario/domain/converter/PullsRepoConverter.kt
@@ -0,0 +1,13 @@
+package com.example.cadastro_de_usuario.domain.converter
+
+import com.example.cadastro_de_usuario.data.dto.PullsListDTO
+import com.example.cadastro_de_usuario.domain.vo.ListPullsVO
+
+class PullRepoConverter {
+
+    fun convert(data: List<PullsListDTO>): List<ListPullsVO> {
+        return data.map { DTO->
+            ListPullsVO(title = DTO.title)
+        }
+    }
+}
