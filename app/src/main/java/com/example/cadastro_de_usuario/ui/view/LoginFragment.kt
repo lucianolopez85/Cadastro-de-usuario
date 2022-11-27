3c745033f8dbd26dab1148b01c4cb6bb5ce02107
lucianolopez85 <lopezz.lucio85@gmail.com>
2022-11-26 22:20:18 -0300

    criado fun hash sha-256

--- app/src/main/java/com/example/cadastro_de_usuario/ui/view/LoginFragment.kt
+++ app/src/main/java/com/example/cadastro_de_usuario/ui/view/LoginFragment.kt
@@ -1,29 +1,49 @@
 package com.example.cadastro_de_usuario.ui.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import androidx.datastore.preferences.protobuf.ByteString
 import androidx.navigation.fragment.findNavController
 import com.example.cadastro_de_usuario.R
+import com.example.cadastro_de_usuario.databinding.FragmentLoginBinding
+import okio.HashingSource.Companion.sha256
+import java.security.MessageDigest
 
-class LoginFragment : Fragment() {
+internal class LoginFragment : Fragment(R.layout.fragment_login) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_login, container, false)
-        val buttonCadastro = view.findViewById<TextView>(R.id.efetuar_cadastro)
+    private val binding by lazy {FragmentLoginBinding.bind(requireView())}
 
-        buttonCadastro.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupButton()
+        getPassword()
+    }
+
+    private fun setupButton() = with(binding){
+
+        buttonLogar.setOnClickListener {
+            TODO()
         }
 
-        return view
+        textRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
+
+    private fun getPassword() {
+        val password = binding.inputPassword.toString()
+        val encryptedPassword = getHash(password.toByteArray())
+        Log.e(encryptedPassword, "testando Cryptography")
+    }
+
+    private fun getHash(inByte: ByteArray): String {
+        val digestedBytes = MessageDigest.getInstance("SHA-256").digest(inByte)
+        return with(StringBuilder()) {
+            digestedBytes.forEach { b -> append(String.format("%02X", b)) }
+            toString().lowercase()
+        }
     }
 
 
